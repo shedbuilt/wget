7f0ebf355e21d43d8bbffbdebca55c1096ac68d3
Auston Stewart <auston@shedbuilt.net>
2018-05-31 11:02:30 -1000

    Updating to v1.19.5 for blank

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Configure
 ./configure --prefix=/usr \
             --sysconfdir=/etc \
-            --with-ssl=openssl
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
+            --with-ssl=openssl &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
